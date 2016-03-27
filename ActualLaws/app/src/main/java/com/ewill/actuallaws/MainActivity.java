936819c48049bb8f8fc4eac57d9c31598e453a3b
e-Will <s-private@mail.ru>
2016-03-28 01:51:47 +0300

    Implement Toolbar

--- ActualLaws/app/src/main/java/com/ewill/actuallaws/MainActivity.java
+++ ActualLaws/app/src/main/java/com/ewill/actuallaws/MainActivity.java
@@ -1,7 +1,32 @@
 package com.ewill.actuallaws;
 
-/**
- * Created by e-Will on 28/03/2016.
- */
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+    private Toolbar _toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        _toolbar = (Toolbar) findViewById(R.id.toolbar);
+        _toolbar.setTitle(R.string.app_name);
+        _toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        _toolbar.inflateMenu(R.menu.menu);
+
+    }
 }
